f6e5fa6c87f9724a23cf2189c24caf3e6a994356
Henry Phan <hphan.cs@gmail.com>
2015-06-12 00:16:50 -0400

    Added routes and corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+
+  root to: 'links#index'
+  
+  # User
+  get 'users/new', to: 'users#new'    # Signup form
+  post 'users', to: 'users#create'    # Create method linked from signup form
+
+  # Link
+  get 'links/new', to: 'links#new'        # New link form
+  post 'links/', to: 'links#create'       # Create link form action
+  get 'link/:id', to: 'links#direct'      # Redirect link to supplied url
+  get 'links', to: 'links#index'          # Links index/homepage
+  delete 'link/:id', to: 'links#destory'  # Deletes link and its comments
+
+  # Comments
+  get 'link/:id/comments', to: 'comments#show'    # Link, comments, comment form
+  post 'link/:id/comments', to: 'comments#create' # Create comment form action
+  delete 'link/:link_id/comment/:comment_id', to: 'comments#destroy' # Delete comment
+
+  # SubLinkedit  
+  get 'l/new', to: 'sublinkedit#new'      # New sublinkedit form
+  post 'l', to: 'sublinkedit#create'      # Creates a new sublinkedit
+  get 'l/:name', to: 'sublinkedit#index'  # SubLinkedit links index
+
+  # Sessions
+  get 'users/login', to: 'sessions#login'       # Login page
+  post 'users/session', to: 'sessions#create'   # Sets session ID
+  delete 'users/session', to: 'session#destory' # Removes session ID
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
