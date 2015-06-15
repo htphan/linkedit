57e16482d532097957fba0c536350144af585d3d
WebEpic <randy@randyjmitchell.com>
2015-06-15 15:02:34 -0400

    added devise gem and dropped Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612001536) do
+ActiveRecord::Schema.define(version: 20150615190030) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150612001536) do
     t.string   "title"
     t.string   "url"
     t.integer  "user_id"
-
     t.integer  "sublinkedit_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
@@ -33,14 +32,6 @@ ActiveRecord::Schema.define(version: 20150612001536) do
 
   create_table "sublinkedits", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
